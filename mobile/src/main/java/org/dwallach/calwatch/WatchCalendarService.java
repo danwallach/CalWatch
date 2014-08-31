da96a9e0ac2b2a98935628bf8e8b30e4d4acb6b4
Dan Wallach <dwallach@cs.rice.edu>
2014-08-30 22:10:50 -0500

    watch can now ping the phone and the phone can respond. Huzzah!

--- mobile/src/main/java/org/dwallach/calwatch/WatchCalendarService.java
+++ mobile/src/main/java/org/dwallach/calwatch/WatchCalendarService.java
@@ -1,16 +1,23 @@
 package org.dwallach.calwatch;
 
+import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class WatchCalendarService extends WearableListenerService {
+public class WatchCalendarService extends Service implements MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private final String TAG = "WatchCalendarService";
 
     private static WatchCalendarService singletonService;
@@ -18,13 +25,27 @@ public class WatchCalendarService extends WearableListenerService {
     private ClockFaceStub clockFaceStub;
     private CalendarFetcher calendarFetcher;
 
+    private GoogleApiClient mGoogleApiClient;
+
+    private void initGoogle() {
+        if(mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this). // TODO probably need to change this to PhoneActivity
+                    addApi(Wearable.API).
+                    addConnectionCallbacks(this).
+                    addOnConnectionFailedListener(this).
+                    build();
+            mGoogleApiClient.connect();
+        }
+    }
+
+
     public WatchCalendarService() {
         super();
 
         Log.v(TAG, "starting calendar fetcher");
-        if(singletonService != null) {
+        if (singletonService != null) {
             Log.v(TAG, "whoa, multiple services!");
-            if(calendarFetcher != null)
+            if (calendarFetcher != null)
                 calendarFetcher.haltUpdates();
         }
 
@@ -33,7 +54,7 @@ public class WatchCalendarService extends WearableListenerService {
         // we'd much rather use the *service* than the *activity* here, but that seems
         // not work, for unknown reasons
         PhoneActivity phoneActivity = PhoneActivity.getSingletonActivity();
-        if(phoneActivity != null) {
+        if (phoneActivity != null) {
             wearSender = new WearSender(phoneActivity);
             clockFaceStub = phoneActivity.getClockFace();
         } else {
@@ -44,11 +65,11 @@ public class WatchCalendarService extends WearableListenerService {
         calendarFetcher = new CalendarFetcher(); // automatically allocates a thread and runs
 
         calendarFetcher.addObserver(new Observer() {
-                                        @Override
-                                        public void update(Observable observable, Object data) {
-                                            sendAllToWatch();
-                                        }
-                                    });
+            @Override
+            public void update(Observable observable, Object data) {
+                sendAllToWatch();
+            }
+        });
     }
 
     public ClockFaceStub getClockFace() {
@@ -67,14 +88,14 @@ public class WatchCalendarService extends WearableListenerService {
     // on the calendar's thread, not the UI thread. It's also useful to call from elsewhere
     // when we want to push data to the watch.
     public void sendAllToWatch() {
-        if(wearSender == null) {
+        if (wearSender == null) {
             Log.v(TAG, "no wear sender?!");
             return;
         }
 
         // first, send the events to the local instance on the phone
         ClockFaceStub clockFaceStub = getClockFace();
-        if(clockFaceStub == null)
+        if (clockFaceStub == null)
             Log.v(TAG, "nowhere to send updated calendar events, hmm");
         else
             clockFaceStub.setEventList(calendarFetcher.getContent().getWrappedEvents());
@@ -86,8 +107,9 @@ public class WatchCalendarService extends WearableListenerService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
         Log.v(TAG, "service starting!");
-        // handleCommand(intent);
+
         // We want this service to continue running until it is explicitly
         // stopped, so return sticky.
         return START_STICKY;
@@ -95,12 +117,14 @@ public class WatchCalendarService extends WearableListenerService {
 
     @Override
     public void onCreate() {
+        super.onCreate();
         Log.v(TAG, "service created!");
+        initGoogle();
     }
 
     @Override
     public void onDestroy() {
-        // Cancel the persistent notification.
+        super.onDestroy();
         Log.v(TAG, "service destroyed!");
     }
 
@@ -109,7 +133,6 @@ public class WatchCalendarService extends WearableListenerService {
     // Very, very helpful: http://www.doubleencore.com/2014/07/create-custom-ongoing-notification-android-wear/
     //
 
-    @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         Log.v(TAG, "message received!");
 
@@ -122,23 +145,22 @@ public class WatchCalendarService extends WearableListenerService {
         }
     }
 
-    /*
-    @Override
     public void onConnected(Bundle connectionHint) {
-        Log.v(TAG, "onConnected!");
+        Log.v(TAG, "Google API connected!");
+
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
     }
 
-    @Override
     public void onConnectionSuspended(int cause) {
         // The connection has been interrupted.
         // Disable any UI components that depend on Google APIs
         // until onConnected() is called.
         Log.v(TAG, "suspended connection!");
+        Wearable.MessageApi.removeListener(mGoogleApiClient, this);
         mGoogleApiClient.disconnect();
         mGoogleApiClient = null;
     }
 
-    @Override
     public void onConnectionFailed(ConnectionResult result) {
         // This callback is important for handling errors that
         // may occur while attempting to connect with Google.
@@ -146,18 +168,23 @@ public class WatchCalendarService extends WearableListenerService {
         // More about this in the next section.
 
         Log.v(TAG, "lost connection!");
+        Wearable.MessageApi.removeListener(mGoogleApiClient, this);
         mGoogleApiClient.disconnect();
         mGoogleApiClient = null;
     }
-    */
 
     public void onPeerConnected(Node peer) {
-        Log.v(TAG, "phone is connected!, "+peer.getDisplayName());
+        Log.v(TAG, "phone is connected!, " + peer.getDisplayName());
     }
 
     public void onPeerDisconnected(Node peer) {
-        Log.v(TAG, "phone is disconnected!, "+peer.getDisplayName());
+        Log.v(TAG, "phone is disconnected!, " + peer.getDisplayName());
     }
 
-
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        Log.e(TAG, "onBind: we should support this");
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
