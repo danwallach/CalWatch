957c745148430272c30f4ea4f70743531c16119c
Dan Wallach <dwallach@cs.rice.edu>
2014-09-09 07:29:36 -0500

    work in progress: general-purpose timing infrastructure in TimeWrapper

--- shared/src/main/java/org/dwallach/calwatch/TimeWrapper.java
+++ shared/src/main/java/org/dwallach/calwatch/TimeWrapper.java
@@ -1,5 +1,8 @@
 package org.dwallach.calwatch;
 
+import android.os.SystemClock;
+import android.util.Log;
+
 import java.util.TimeZone;
 
 /**
@@ -8,6 +11,7 @@ import java.util.TimeZone;
  * to fix the problem.
  */
 public class TimeWrapper {
+    private static final String TAG = "TimeWrapper";
     private static TimeZone tz;
     private static int gmtOffset;
     private static long time;
@@ -24,6 +28,43 @@ public class TimeWrapper {
 
     public static long getGMTTime() { return time; }
     public static long getLocalTime() { return time + gmtOffset; }
+
+    private static long frameStartTime;
+
+    /**
+     * for performance monitoring: call this at the beginning of every screen refresh
+     */
+    public static void frameStart() {
+        frameStartTime = SystemClock.elapsedRealtimeNanos();
+    }
+
+    /**
+     * for performance monitoring: call this at the end of every screen refresh
+     */
+    private static long lastFPSTime = 0;
+    private static int ticks = 0;
+
+    public static void frameEnd() {
+        ticks++;
+
+        long frameEndTime = SystemClock.elapsedRealtimeNanos();
+
+        // first, let's sort out the average frames per second
+        if(lastFPSTime == 0)
+            lastFPSTime = frameEndTime;
+
+        long elapsedTime = frameEndTime - frameStartTime;
+
+        // work in progress: compute statistics
+
+        // if at least five minutes have elapsed, then it's time to print all the things
+        if(frameEndTime - lastFPSTime > 300000000000L) { // 5 * 60 * 10^9
+            float fps = (ticks * 1000000000f) / (frameEndTime - lastFPSTime); // 500 frame * 1000 ms/s / elapsed ms
+            lastFPSTime = frameEndTime;
+            ticks = 0;
+            Log.v(TAG, "FPS: " + Float.toString(fps));
+        }
+    }
 
     static {
         // do this once at startup because why not?
