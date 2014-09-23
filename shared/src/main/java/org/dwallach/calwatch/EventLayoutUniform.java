b39c5064bb92ee1440ca57148df2864e577811fb
Dan Wallach <dwallach@cs.rice.edu>
2014-09-22 21:04:08 -0500

    work in progress: we're about to try out the constraint solver!

--- shared/src/main/java/org/dwallach/calwatch/EventLayoutUniform.java
+++ shared/src/main/java/org/dwallach/calwatch/EventLayoutUniform.java
@@ -20,15 +20,15 @@ public class EventLayoutUniform {
      * non-overlapping layout.
      *
      * @param events list of events
-     * @return maximum level of any calendar event
+     * @return true if it worked, false if it failed
      */
-    public static int go(List<EventWrapper> events) {
+    public static boolean go(List<EventWrapper> events) {
         int i, j, k, nEvents;
         int maxLevelAnywhere = 0;
 
-        if (events == null) return 0;  // degenerate case, shouldn't happen
+        if (events == null) return true; // degenerate case, in which we trivially succeed
         nEvents = events.size();
-        if (nEvents == 0) return 0;    // another degnerate case
+        if (nEvents == 0) return true; // degenerate case, in which we trivially succeed
 
         for (i = 1; i < nEvents; i++) {
             EventWrapper e = events.get(i);
@@ -47,6 +47,8 @@ public class EventLayoutUniform {
             ClVariable[] startLevels = new ClVariable[nEvents];
             ClVariable[] sizes = new ClVariable[nEvents];
 
+            ClLinearExpression sumSizes = new ClLinearExpression(0.0);
+
             j = 0;
             for (i = 0; i < nEvents; i++) {
                 startLevels[i] = new ClVariable("start" + i);
@@ -58,39 +60,60 @@ public class EventLayoutUniform {
 
                 // constraints: add them together and they're still constrained by MAXLEVEL
                 ClLinearExpression levelPlusSize = new ClLinearExpression(startLevels[i]).plus(sizes[i]);
-                ClLinearInequality liq = new ClLinearInequality(levelPlusSize, CL.LEQ, new ClLinearExpression(MAXLEVEL));
+                ClLinearInequality liq = new ClLinearInequality(levelPlusSize, CL.LEQ, new ClLinearExpression(MAXLEVEL), ClStrength.required);
                 solver.addConstraint(liq);
+
+                sumSizes = sumSizes.plus(sizes[i]);
             }
+
+            // constraint: the sum of all the sizes is greater than the maximum it could ever be under the absolute best of cases
+            // (note: ClStrength.weak -- we just want to push things to be greater than the degenerate case of zero-width events)
+            ClLinearInequality sumSizesEq = new ClLinearInequality(sumSizes, CL.GEQ, new ClLinearExpression(MAXLEVEL*nEvents), ClStrength.weak);
+            solver.addConstraint(sumSizesEq);
 
             for (i = 0; i < nEvents; i++) {
                 for (j = i + 1; j < nEvents; j++)
                     if (events.get(i).overlaps(events.get(j))) {
                         // constraint: base level + its size < base level of next dependency
                         ClLinearExpression levelPlusSize = new ClLinearExpression(startLevels[i]).plus(sizes[i]);
-                        ClLinearInequality liq = new ClLinearInequality(levelPlusSize, CL.LEQ, startLevels[j]);
+                        ClLinearInequality liq = new ClLinearInequality(levelPlusSize, CL.LEQ, startLevels[j], ClStrength.required);
                         solver.addConstraint(liq);
 
                         // weak constraint: constrained segments should have the same size
-                        ClLinearEquation eqSize = new ClLinearEquation(sizes[i], new ClLinearExpression(sizes[j]), ClStrength.weak);
+                        ClLinearEquation eqSize = new ClLinearEquation(sizes[i], new ClLinearExpression(sizes[j]), ClStrength.medium);
                         solver.addConstraint(eqSize);
                     }
-
             }
 
-        // TODO: if this blows up, we should fall back to the non-uniform layout contraption
+            // and... away we go!
+            solver.solve();
 
+            Log.v(TAG, "solver completed!");
+
+            for(i=0; i<nEvents; i++) {
+                EventWrapper e = events.get(i);
+                double start = startLevels[i].value();
+                double size = sizes[i].value();
+
+                e.setMinLevel((int) start);
+                e.setMaxLevel((int) (start + size));
+
+                Log.v(TAG, "event " + i + ": minLevel(" + start + "), maxLevel(" + start+size + ")");
+            }
         } catch (ExCLInternalError e) {
             Log.e(TAG, e.toString());
+            return false;
         } catch (ExCLRequiredFailure e) {
             Log.e(TAG, e.toString());
+            return false;
         } catch (ExCLNonlinearExpression e) {
             Log.e(TAG, e.toString());
+            return false;
+        } finally {
+            long nanoStop = SystemClock.elapsedRealtimeNanos();
+            Log.v(TAG, "Constraing solver, completed: " + (Double.toString((nanoStop - nanoStart) / 1000000.0)) + " ms");
         }
 
-        long nanoStop = SystemClock.elapsedRealtimeNanos();
-
-        Log.v(TAG, "Constraing solver, completed: " + (Double.toString((nanoStop - nanoStart) / 1000000.0)) + " ms");
-
-        return maxLevelAnywhere;
+        return true;
     }
 }
