e77cc8f9f520e345e319f243747acca3aaac2808
Dan Wallach <dwallach@cs.rice.edu>
2014-11-10 11:47:02 -0600

    copyright notices added everywhere

--- shared/src/main/java/org/dwallach/calwatch/LockWrapper.java
+++ shared/src/main/java/org/dwallach/calwatch/LockWrapper.java
@@ -1,3 +1,9 @@
+/*
+ * CalWatch
+ * Copyright (C) 2014 by Dan Wallach
+ * Home page: http://www.cs.rice.edu/~dwallach/calwatch/
+ * Licensing: http://www.cs.rice.edu/~dwallach/calwatch/licensing.html
+ */
 package org.dwallach.calwatch;
 
 import java.util.concurrent.locks.Lock;
