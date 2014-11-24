f9558e81959e8e2754844b2b49169c30b46dfb16
Dan Wallach <dwallach@cs.rice.edu>
2014-11-24 16:03:22 -0600

    removed unnecessary resource file & value

--- mobile/src/main/java/org/dwallach/calwatch/PhoneActivity.java
+++ mobile/src/main/java/org/dwallach/calwatch/PhoneActivity.java
@@ -249,10 +249,6 @@ public class PhoneActivity extends Activity implements Observer {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
